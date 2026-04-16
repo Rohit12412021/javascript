e21d377dee74e92257c3eab0959e96ae99738ee8
Rohit12412021 <rakffffsh9467543090@gmail.com>
2026-04-16 11:45:43 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+ "use strict"  //use js code acc to new version
+//alert(3+3) //only use in broweser engine not in nodejs
+//datatypes
+console.log("rohit ") //string datatype
+ let age=19  //integer datatype
+ let isloggedIn=false // boolean datatype
+ //check datatype
+ console.log(typeof(isloggedIn))
