9a5942a5d126a0e8f4d4d72b4808aae093880111
Rohit12412021 <rakffffsh9467543090@gmail.com>
2026-04-16 12:03:12 +0530

    DataTypes Conversion

---
+++ datatypes2.js
@@ -0,0 +1,19 @@
+let score="33ab";
+console.log(typeof(score))
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)//given nan because the particular string cant change into number
+let number=null
+console.log(number)//gives 0
+let number1=undefined
+console.log(number1)//give nan again
+//
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)//here 1 change into true also meaningful
+//1-->true  0-->false
+let somenumber=33;
+let stringNumber=String(somenumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
