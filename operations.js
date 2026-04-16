fedfb4cfbd01fcde7d614f50753d233a4f77dc51
Rohit12412021 <rakffffsh9467543090@gmail.com>
2026-04-16 12:22:21 +0530

    operations in js

---
+++ operations.js
@@ -0,0 +1,22 @@
+let value =3
+let negvalue=-value
+console.log(negvalue)
+console.log(2+2);
+console.log(2*3)
+console.log(2-2)
+console.log(2**3)//2^3
+console.log(2/3)
+console.log(2%3)//remainder
+let str1="rohit"
+let str2="gourav"
+let str3=str1+str2//both string attached
+console.log(str3)
+//
+console.log("1"+2+3) //still give 123
+console.log(1+2+"2")//here it gives 32
+console.log(true)//gives true
+console.log(+true)// gives 1
+
+let x=5
+console.log(x++)//post increment//gives 5 baad mein increase kreag
+console.log(++x) //preincrement   increase immideately gives 7
