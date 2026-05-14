28399da8e42c765d00000e13afc12f19718d00e1
Rohit12412021 <rakffffsh9467543090@gmail.com>
2026-05-14 08:00:04 +0530

    Dom basics

---
+++ Basics3/Domflow.js
@@ -0,0 +1,17 @@
+//DOCUMENT OBJECT MODEL -->TREE STRUCTURE
+
+//Window-->Document-->Html
+
+//Html->1) Head    2)Body
+
+//head-->title meta 
+
+//title-->text node  meta-->attribute
+
+//body->div
+//div->(h1 and p) are child
+//h1->attribute and textnode
+//p->textnode
+//----------------------------------------------------------------------
+//defination of dom (for interviews)==>it is the programming interface which repersent html webpages in the form of tree of objects
+
